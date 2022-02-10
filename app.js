643488d00f332b679dd9002b841f6603e789b72c
Abebe Gietaneh <abebegth@gmail.com>
2022-02-10 17:30:46 +0300

    get request for tours data

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const express = require("express");
 
 const app = express();
@@ -6,9 +7,22 @@ app.get('/', (req, res) =>{
     res.status(200).json({message: "Hello from the server...", app: "Tour and Travel"})
 });
 
-app.post('/', (req, res) =>{
-    res.send('Checking the post method...')
-})
+// app.post('/', (req, res) =>{
+//     res.send('Checking the post method...')
+// })
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/json-data/data/tours-simple.json`));
+// console.log(tours);
+
+app.get('/api/v1/tours', (req, res) =>{
+    res.status(200).json({
+        status: "Success",
+        result: tours.length,
+        data: {
+            tours: tours
+        }
+    })
+});
 
 const port = 3000;
 app.listen(port, ()=>{
