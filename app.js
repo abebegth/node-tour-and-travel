b685e59e393e6cc26283fe3ae215de9918121fb0
Abebe Gietaneh <abebegth@gmail.com>
2022-02-10 10:38:02 +0300

    express setup & basic routing

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) =>{
-    res.status(200).send("Hello from the server...")
+    res.status(200).json({message: "Hello from the server...", app: "Tour and Travel"})
 });
+
+app.post('/', (req, res) =>{
+    res.send('Checking the post method...')
+})
 
 const port = 3000;
 app.listen(port, ()=>{
